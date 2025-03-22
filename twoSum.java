f886428ef90a87809ae3dcda5b3a43edb8c037e3
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2025-03-22 10:55:47 +0530

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -1,4 +1,4 @@
-//Naive
+//Naive - TC: O(n2), SC: O(1)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i = 0; i<nums.length-1; i++) {
@@ -7,6 +7,22 @@ class Solution {
                     return new int[]{i, j};
                 }
             }
+        }
+        return null;
+    }
+}
+
+//TC: O(n)
+//SC: O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i<nums.length;i++) {
+            if(map.get(target - nums[i])!=null) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
         }
         return null;
     }
