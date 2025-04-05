7f6af053e07b1427e7226195a94f3dc4d44f3d90
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2025-04-05 11:43:00 +0530

    Update lengthOfLongestSubstring.java

--- lengthOfLongestSubstring.java
+++ lengthOfLongestSubstring.java
@@ -24,3 +24,26 @@ class Solution {
         return max;
     }
 }
+
+//698 / 987 testcases passed - Skipped to debug to save time
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0) return 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 1;
+
+        int i = 0, j = 0;
+
+        while(j<s.length()) {
+            if(map.containsKey(s.charAt(j)) && map.get(s.charAt(j)) > i) {
+                i = map.get(s.charAt(j))+1;
+            }
+            map.put(s.charAt(j), j);
+
+            max = Math.max(max, j-i+1);
+            j++;
+        }
+        return max;
+    }
+}
