9b1121bc761686892ec09b6a95da44f8e62c1eed
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2025-04-05 11:54:03 +0530

    Update missingNumber.java

--- missingNumber.java
+++ missingNumber.java
@@ -12,3 +12,34 @@ class Solution {
         return xor;
     }
 }
+
+
+//tricky approach
+class Solution {
+    public int missingNumber(int[] nums) {
+        int temp = -1;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) continue;
+
+            int index = Math.abs(nums[i])-1;
+
+            if(nums[index] == 0) {
+                temp = Math.abs(nums[i]);
+            } else {
+                nums[index] = nums[index] * -1;
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0 && temp == i+1) {
+                continue;
+            }
+            if(nums[i]>=0) {
+                return i+1;
+            }
+        }
+
+        return 0;
+    }
+}
