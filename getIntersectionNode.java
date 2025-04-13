ef19085ab7b977381e6549f76c95d4abb3849c09
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2025-04-13 10:23:14 +0530

    Update getIntersectionNode.java

--- getIntersectionNode.java
+++ getIntersectionNode.java
@@ -1,3 +1,4 @@
+//fair unfair race method
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) return null;
@@ -7,5 +8,57 @@ public class Solution {
             b = (b == null) ? headA : b.next;
         }
         return a;
+    }
+}
+
+
+
+
+//count method
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int firstCount = 0;
+        int secondCount = 0;
+
+        ListNode curr = headA;
+
+        while(curr!=null) {
+            firstCount++;
+            curr = curr.next;
+        }
+
+        curr = headB;
+        while(curr!=null) {
+            secondCount++;
+            curr = curr.next;
+        }
+
+        ListNode first = headA, second = headB;
+
+
+        if(firstCount > secondCount) {
+            //skip starting from first linked list
+            first = skipNodes(first, firstCount - secondCount);
+        } else {
+             //skip starting from second linked list
+             second = skipNodes(second, secondCount - firstCount );
+        }
+
+        while(first!= null && second!=null) {
+            if(first == second) return first;
+
+            first = first.next;
+            second = second.next;
+        }
+
+        return null;
+    }
+
+    private ListNode skipNodes(ListNode node, int countToSkip) {
+            while(countToSkip>0) {
+                node = node.next;
+                countToSkip--;
+            }
+            return node;
     }
 }
