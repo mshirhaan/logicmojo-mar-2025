95e3b0b5e136768b264dcdf0d6c90f38985d2bcb
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2025-04-20 11:38:05 +0530

    Update LFU.js

--- LFU.js
+++ LFU.js
@@ -168,148 +168,162 @@ class LFUCache {
     
 };
 
-//Chatgpt converted java code works fine
+//Chatgpt converted map = {} to new Map(), and it works
 
-class LFUCache {
+class DoublyLinkedList {
+    head;
+    tail;
+    size = 0;
 
-    private int capacity;
-    private int lowestFrequencyCount = 1;
-    private Map<Integer, Node> nodeMap;
-    private Map<Integer, DoublyLinkedList> frequencyMap;
+    use(node) {
+        if (head == node) return;
 
-    public LFUCache(int capacity) {
-        this.capacity = capacity;
-        this.nodeMap = new HashMap<>();
-        this.frequencyMap = new HashMap<>();
+        if (node == tail) {
+            tail = tail.prev;
+            tail.next = null;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+        node.next = head;
+        head.prev = node;
+        head = node;
     }
 
-    // Get the value associated with the key
-    public int get(int key) {
-        if (!nodeMap.containsKey(key)) {
+    evict() {
+        let key = this.tail.key;
+        if (this.size == 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = this.tail.prev;
+            this.tail.next = null;
+        }
+        this.size--;
+        return key;
+    }
+
+    put(node) {
+        if (this.size == 0) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            node.next = this.head;
+            this.head.prev = node;
+            this.head = node;
+        }
+        this.size++;
+    }
+
+    remove(node) {
+        if (this.size == 1) {
+            this.head = null;
+            this.tail = null;
+            node.next = null;
+            node.prev = null;
+        } else if (this.head == node) {
+            this.head = node.next;
+            this.head.prev = null;
+            node.next = null;
+            node.prev = null;
+        } else if (this.tail == node) {
+            this.tail = node.prev;
+            this.tail.next = null;
+            node.next = null;
+            node.prev = null;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            node.next = null;
+            node.prev = null;
+        }
+        this.size--;
+    }
+}
+
+class Node {
+    key;
+    value;
+    count = 1;
+    next = null;
+    prev = null;
+
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class LFUCache {
+    capacity = 0;
+    lowestFrequencyCount = 1;
+    nodeMap = new Map();   // Changed from {} to Map()
+    frequencyMap = new Map(); // Changed from {} to Map()
+
+    constructor(capacity) {
+        this.capacity = capacity;
+    }
+
+    get(key) {
+        if (!this.nodeMap.has(key)) {
             return -1;
         }
 
-        Node node = nodeMap.get(key);
+        let node = this.nodeMap.get(key);
         node.count++;
 
-        // Remove the node from the old frequency list
-        DoublyLinkedList oldDll = frequencyMap.get(node.count - 1);
+        let oldDll = this.frequencyMap.get(node.count - 1);
         oldDll.remove(node);
-
-        // If the old frequency list is empty and it was the lowest frequency, update the lowestFrequencyCount
-        if (oldDll.size == 0 && node.count - 1 == lowestFrequencyCount) {
-            lowestFrequencyCount++;
+        if (oldDll.size == 0 && node.count - 1 == this.lowestFrequencyCount) {
+            this.lowestFrequencyCount++;
         }
 
-        // Insert the node into the new frequency list
-        if (!frequencyMap.containsKey(node.count)) {
-            frequencyMap.put(node.count, new DoublyLinkedList());
+        if (!this.frequencyMap.has(node.count)) {
+            this.frequencyMap.set(node.count, new DoublyLinkedList());
         }
-        DoublyLinkedList newDll = frequencyMap.get(node.count);
+        let newDll = this.frequencyMap.get(node.count);
         newDll.put(node);
 
         return node.value;
     }
 
-    // Put a new key-value pair into the cache
-    public void put(int key, int value) {
-        if (capacity == 0) return;
-
-        if (nodeMap.containsKey(key)) {
-            Node node = nodeMap.get(key);
+    put(key, value) {
+        if (this.nodeMap.has(key)) {
+            let node = this.nodeMap.get(key);
             node.value = value;
             node.count++;
 
-            // Remove the node from the old frequency list
-            DoublyLinkedList oldDll = frequencyMap.get(node.count - 1);
+            let oldDll = this.frequencyMap.get(node.count - 1);
             oldDll.remove(node);
-
-            // If the old frequency list is empty and it was the lowest frequency, update the lowestFrequencyCount
-            if (oldDll.size == 0 && node.count - 1 == lowestFrequencyCount) {
-                lowestFrequencyCount++;
+            if (oldDll.size == 0 && node.count - 1 == this.lowestFrequencyCount) {
+                this.lowestFrequencyCount++;
+                this.frequencyMap.delete(node.count - 1);  // Cleanup the empty frequency list
             }
 
-            // Insert into the new frequency list
-            if (!frequencyMap.containsKey(node.count)) {
-                frequencyMap.put(node.count, new DoublyLinkedList());
+            if (!this.frequencyMap.has(node.count)) {
+                this.frequencyMap.set(node.count, new DoublyLinkedList());
             }
-            DoublyLinkedList newDll = frequencyMap.get(node.count);
+            let newDll = this.frequencyMap.get(node.count);
             newDll.put(node);
         } else {
-            if (nodeMap.size() == capacity) {
-                // Evict the least frequently used node
-                DoublyLinkedList dll = frequencyMap.get(lowestFrequencyCount);
-                int evictedKey = dll.evict();
-                nodeMap.remove(evictedKey);
+            // Cache is full, evict the least frequently used node
+            if (this.nodeMap.size == this.capacity) {
+                let dll = this.frequencyMap.get(this.lowestFrequencyCount);
+                let evictedKey = dll.evict();
+                this.nodeMap.delete(evictedKey);  // Remove evicted node from nodeMap
             }
 
-            // Insert the new node
-            Node node = new Node(key, value);
-            nodeMap.put(key, node);
+            let node = new Node(key, value);
+            this.nodeMap.set(key, node);
 
-            if (!frequencyMap.containsKey(1)) {
-                frequencyMap.put(1, new DoublyLinkedList());
+            if (!this.frequencyMap.has(1)) {
+                this.frequencyMap.set(1, new DoublyLinkedList());
             }
-            lowestFrequencyCount = 1;
-            DoublyLinkedList dll = frequencyMap.get(1);
+            this.lowestFrequencyCount = 1;
+            let dll = this.frequencyMap.get(1);
             dll.put(node);
         }
     }
 }
 
-// Doubly Linked List to store nodes of the same frequency
-class DoublyLinkedList {
-    Node head;
-    Node tail;
-    int size;
 
-    public DoublyLinkedList() {
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
-        head.next = tail;
-        tail.prev = head;
-        size = 0;
-    }
-
-    // Adds a node to the front of the list
-    public void put(Node node) {
-        node.next = head.next;
-        node.prev = head;
-        head.next.prev = node;
-        head.next = node;
-        size++;
-    }
-
-    // Removes a node from the list
-    public void remove(Node node) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-        node.next = null;
-        node.prev = null;
-        size--;
-    }
-
-    // Evicts the least recently used node (tail)
-    public int evict() {
-        Node evicted = tail.prev;
-        remove(evicted);
-        return evicted.key;
-    }
-}
-
-// Node class representing each key-value pair in the cache
-class Node {
-    int key;
-    int value;
-    int count;
-    Node next;
-    Node prev;
-
-    public Node(int key, int value) {
-        this.key = key;
-        this.value = value;
-        this.count = 1;
-        this.next = null;
-        this.prev = null;
-    }
-}
