0af0afeecd0fbf35852fb215cb5777e3f3461fb7
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2025-06-15 09:53:57 +0530

    Create TopoSort-Kahns-BFS.js

---
+++ TopoSort-Kahns-BFS.js
@@ -0,0 +1,44 @@
+class Solution {
+    topoSort(V, edges) {
+        // code here
+        let map = {}
+        let indegrees = []
+        let result = []
+        
+        for(let i = 0; i<V; i++) {
+            map[i] = new Set();
+            indegrees[i] = 0;
+        }
+        
+        for(let edge of edges) {
+            let [from, to] = edge;
+            map[from].add(to);
+            indegrees[to]++;
+        }
+        
+        let queue = []
+        
+        for(let i = 0; i<V; i++) {
+            if(indegrees[i] == 0) {
+                queue.push(i);
+            }
+        }
+        
+        while(queue.length) {
+            let vertex = queue.shift();
+            result.push(vertex);
+
+            for(let dependent of map[vertex]) {
+                indegrees[dependent]--;
+                if(indegrees[dependent] == 0) {
+                    queue.push(dependent);
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+
+let sol = new Solution()
+sol.topoSort(6, [[1, 3], [2, 3], [4, 1], [4, 0], [5, 0], [5,2]])
