69971732b380c806fb50ba239a5e5be3781871c7
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2025-07-19 12:20:56 +0530

    Update maxProfit2.js

--- maxProfit2.js
+++ maxProfit2.js
@@ -1,3 +1,14 @@
+//Simple approach
+var maxProfit = function(prices) {
+    let profit = 0;
+
+    for(let i = 1; i<prices.length; i++) {
+        profit+= Math.max(0, prices[i] - prices[i-1]);
+    }
+    return profit;
+};
+
+//MEMO DP
 var maxProfit = function(prices) {
     function helper(i, canIBuy, memo = {} ) {
         if(i == prices.length) {
